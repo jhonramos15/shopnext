c5596e57df0ddd91915502c6e797ce15f86c0ca1
BrayanArdilaBG <brayan.stiven.ardila.espana@gmail.com>
2025-04-15 12:33:56 -0500

    Login y Sign-Up

---
+++ shopnexs/js/script.js
@@ -0,0 +1,8 @@
+let indice = 0;
+const slides = document.querySelectorAll('.slide');
+
+function cambiarSlide(direccion) {
+  slides[indice].classList.remove('activo');
+  indice = (indice + direccion + slides.length) % slides.length;
+  slides[indice].classList.add('activo');
+}
